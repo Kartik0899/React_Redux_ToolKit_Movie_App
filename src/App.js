da21dcbfec383e2231f3ec21cc1de81e864ef13c
Kartik0899 <jaybudhraja08@gmail.com>
2023-01-29 02:33:01 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.scss';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from "./components/Home/Home";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+{/* <Route path="/" element={<Home />} />
+<Route path="/movie/:imdbID" element={<MovieDetail />} />
+<Route element={<PageNotFound />} /> */}
---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,3 @@
+// https://www.omdbapi.com/?i=tt3896198&apikey=416a9d0
+
+export const APIKEY = '416a9d0';
